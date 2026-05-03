978eb5ab04a38b728580c890e74ee6abc95f99d0
charlesf-git <charlesf.git@gmail.com>
2026-05-03 21:44:04 +0800

    feature: set VLC as default app for video and audio MIME types

--- home.nix
+++ home.nix
@@ -15,4 +15,29 @@ in {
       init.defaultBranch = "main";
     };
   };
+
+  # Set VLC as the default app for video and audio files
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      # Video
+      "video/mp4"       = "org.videolan.VLC.desktop";
+      "video/mkv"       = "org.videolan.VLC.desktop";
+      "video/x-matroska"= "org.videolan.VLC.desktop";
+      "video/mpeg"      = "org.videolan.VLC.desktop";
+      "video/quicktime" = "org.videolan.VLC.desktop";
+      "video/x-msvideo" = "org.videolan.VLC.desktop";  # .avi
+      "video/webm"      = "org.videolan.VLC.desktop";
+      "video/ogg"       = "org.videolan.VLC.desktop";
+
+      # Audio
+      "audio/mpeg"      = "org.videolan.VLC.desktop";  # .mp3
+      "audio/mp4"       = "org.videolan.VLC.desktop";  # .m4a
+      "audio/flac"      = "org.videolan.VLC.desktop";
+      "audio/ogg"       = "org.videolan.VLC.desktop";
+      "audio/x-wav"     = "org.videolan.VLC.desktop";
+      "audio/aac"       = "org.videolan.VLC.desktop";
+      "audio/opus"      = "org.videolan.VLC.desktop";
+    };
+  };
 }
