525412944c040eafe9e1632465f9b1efb8e49042
charlesf-git <charlesf.git@gmail.com>
2026-05-03 21:49:30 +0800

    feature: replace Console with GNOME Terminal

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -13,7 +13,29 @@
       gnome-music
       gnome-tour
       simple-scan # Document Scanner
+      gnome-console
     ];
+
+    # Install GNOME apps
+    environment.systemPackages = with pkgs; [
+      gnome-terminal
+    ];
+
+    # Set GNOME Terminal as the default terminal via dconf
+    home-manager.users.${settings.username}.dconf.settings = {
+      "org/gnome/desktop/default-applications/terminal" = {
+        exec = "gnome-terminal";
+        exec-arg = "-x";
+      };
+
+      # Optional: set a sensible default profile
+      # Run `dconf watch /` in a terminal while changing settings
+      # in GNOME Terminal to discover the profile UUID and keys
+      "org/gnome/terminal/legacy" = {
+        default-show-menubar = false;
+        theme-variant = "dark";
+      };
+    };
 
     # Flatpak apps only relevant on GNOME
     services.flatpak.packages = [
